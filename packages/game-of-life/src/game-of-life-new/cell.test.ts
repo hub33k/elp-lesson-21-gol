0ffe533ec96c09880255765a9ddfadc27924b511
hub33k <hub33k@yahoo.com>
2022-04-19 23:07:27 +0200

    tests: should return initial cell state

--- packages/game-of-life/src/game-of-life-new/cell.test.ts
+++ packages/game-of-life/src/game-of-life-new/cell.test.ts
@@ -1,5 +1,6 @@
 describe('cell', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
+  it('should return initial cell state', () => {
+    const cell = new Cell(0, []);
+    expect(cell.getState()).toBe(0);
   });
 });
