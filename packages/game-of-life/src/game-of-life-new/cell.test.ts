0885f8fd13c286787cbc07ee35f18544b4097ca9
hub33k <hub33k@yahoo.com>
2022-04-19 23:18:51 +0200

    feat: returns initial cell state

--- packages/game-of-life/src/game-of-life-new/cell.test.ts
+++ packages/game-of-life/src/game-of-life-new/cell.test.ts
@@ -1,3 +1,13 @@
+type ICellState = 1 | 0;
+
+class Cell {
+  constructor(private state: ICellState, private neighbours: Array<Cell>) {}
+
+  public getState(): ICellState {
+    return this.state;
+  }
+}
+
 describe('cell', () => {
   it('should return initial cell state', () => {
     const cell = new Cell(0, []);
