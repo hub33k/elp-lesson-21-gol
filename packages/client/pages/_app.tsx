c18a70a6f6a038f102240d97e5fbd5d5be64b303
hub33k <hub33k@yahoo.com>
2022-04-19 21:30:53 +0200

    feat: add basic front app

--- packages/client/pages/_app.tsx
+++ packages/client/pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import config from '~/config';
+import '~/styles/tailwind.css';
+import '~/styles/global.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,13 +1,92 @@
 import type { NextPage } from 'next';
 import { GameOfLife } from 'game-of-life';
+import { useEffect, useState } from 'react';
 
 const HomePage: NextPage = () => {
-  const gol = new GameOfLife(3, 3);
-  console.log(gol);
+  const [game, setGame] = useState(new GameOfLife(10, 10));
+  const [board, setBoard] = useState(game.getBoard());
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    // Example 1
+    game.setCell(0, 1);
+    game.setCell(1, 2);
+    game.setCell(2, 0);
+    game.setCell(2, 1);
+    game.setCell(2, 2);
+
+    // Example 2
+    // game.setCell(0, 1);
+    //
+    // game.setCell(1, 0);
+    // game.setCell(1, 1);
+    // game.setCell(1, 2);
+    //
+    // game.setCell(2, 0);
+    // game.setCell(2, 1);
+    // game.setCell(2, 2);
+
+    setIsLoaded(true);
+  }, []);
 
   return (
     <>
-      <h1>Home page</h1>
+      <h1 className="text-3xl font-bold underline">Game of Life</h1>
+
+      {isLoaded && (
+        <>
+          <table className={'border-collapse border border-slate-500 w-64'}>
+            <tbody>
+              {board.map((row, rowIndex) => {
+                return (
+                  <tr key={rowIndex} className={'row'}>
+                    {row.map((cell, cellIndex) => {
+                      return (
+                        <td
+                          key={cellIndex}
+                          className={`cell border border-slate-700 text-center w-10 h-5 ${
+                            cell
+                              ? 'bg-slate-700 text-white border-slate-300'
+                              : 'bg-white'
+                          }`}
+                        >
+                          {' '}
+                        </td>
+                      );
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+
+          <button
+            className={
+              'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-32'
+            }
+            onClick={() => {
+              game.tick();
+              setBoard(game.getBoard());
+            }}
+          >
+            tick
+          </button>
+
+          <button
+            className={
+              'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-32'
+            }
+            onClick={() => {
+              const intervalId = setInterval(() => {
+                game.tick();
+                setBoard(game.getBoard());
+              }, 500);
+            }}
+          >
+            start
+          </button>
+        </>
+      )}
     </>
   );
 };
