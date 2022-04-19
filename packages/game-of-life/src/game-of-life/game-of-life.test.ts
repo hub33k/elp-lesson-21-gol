d1839822c61598d9ab5383184c58fc732a9bc084
hub33k <hub33k@yahoo.com>
2022-04-19 13:05:52 +0200

    tests: should return correct number of neighbors

--- packages/game-of-life/src/game-of-life/game-of-life.test.ts
+++ packages/game-of-life/src/game-of-life/game-of-life.test.ts
@@ -43,4 +43,20 @@ describe('GameOfLife', () => {
     // then
     expect(gameOfLife.getCell(1, 2)).toEqual(1);
   });
+
+  it('should return correct number of neighbors', () => {
+    // when
+    const gameOfLife = new GameOfLife(3, 3);
+
+    // given
+    // [0, 0, 0],
+    // [0, 0, 0],
+    // [0, 0, 0],
+    gameOfLife.setCell(0, 0);
+    gameOfLife.setCell(0, 1);
+    gameOfLife.setCell(0, 2);
+
+    // then
+    expect(gameOfLife.getNeighbors(0, 1)).toEqual(2);
+  });
 });
