411d1189ea79b3608ded2790a6e5d2945725a4c7
hub33k <hub33k@yahoo.com>
2022-04-19 14:36:19 +0200

    feat: handle cell with 3 neighbors

--- packages/game-of-life/src/game-of-life/game-of-life.ts
+++ packages/game-of-life/src/game-of-life/game-of-life.ts
@@ -59,6 +59,8 @@ export class GameOfLife {
         const neighborCount = this.getNeighbors(i, j);
         if (neighborCount < 2) {
           tempBoard2[i][j] = 0;
+        } else if (neighborCount === 3) {
+          tempBoard2[i][j] = 1;
         }
       });
     });
